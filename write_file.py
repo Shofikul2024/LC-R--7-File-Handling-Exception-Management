f46ff75f347d9cae9badd3367c7676d05b846d03
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-09-07 02:29:26 +0600

    added read

---
+++ write_file.py
@@ -0,0 +1,24 @@
+# with open("index.html", "w") as file:
+#     file.write("<h1> learn python  </h1>")
+#
+
+
+#
+# note=open("home.html", "w")
+# note.write("<h1> learn python  </h1>")
+
+
+
+
+
+
+
+
+# specific   folder ar maje   aibave   file write korte pari
+
+# with open("assets/index.html", "w") as file:
+#     file.write("<h1> folder  ar  maje  login file create korechi and tar maje liktaci  </h1>")
+
+
+import os
+os.rename("assets/example.text", "assets/new.txt")
